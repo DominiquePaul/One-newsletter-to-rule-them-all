44e126b472758b9256286a95132a9348fc0f309b
Dominique Paul <dominique.c.a.paul@gmail.com>
2025-01-18 16:43:26 +0100

    update data model

--- scraping_tool/article.py
+++ scraping_tool/article.py
@@ -1,9 +1,26 @@
-from dataclasses import dataclass
+from pydantic import BaseModel
+from datetime import datetime
+from pydantic import field_validator
 
-@dataclass
-class Article():
-    headline: str
-    description: str
+class Article(BaseModel):
+    heading: str
+    subheading: str
     date: str
-    link_url: str
+    url: str
     content: str
+
+    @field_validator('date')
+    def validate_date(cls, v):
+        try:
+            # Try to parse the date string to verify it's valid
+            datetime.strptime(v, '%Y-%m-%d')
+            return v
+        except ValueError:
+            raise ValueError('Date must be in YYYY-MM-DD format')
+        
+
+class ArticleChunk(BaseModel):
+    article_url: str
+    embedding: list[float]
+    content: str
+    num_characters: int
