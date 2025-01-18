c785f8fbec6c5c7c4ecd96bfe3fa017726da2d66
marcandrewessner <94740952+marcandrewessner@users.noreply.github.com>
2025-01-18 16:37:04 +0100

    nzz

---
+++ scraping_tool/nzz.py
@@ -0,0 +1,31 @@
+# This file is used to scrape the NZZ articles
+from trafilatura import fetch_url, extract
+from feedparser import parse as parse_feed
+from time import strftime
+from article import Article
+
+def main():
+
+    nzz_feed_link = "https://www.nzz.ch/technologie.rss"
+    nzz_feed = parse_feed(nzz_feed_link)
+    
+    for article in nzz_feed.entries:
+        page_title = article.title
+        page_description = article.description
+        page_link = article.link
+        page_timestamp = article.published_parsed
+        page_timestamp = strftime("%Y-%m-%d %H:%M:%S", page_timestamp)
+        # Stop at only one article
+        raw_article = fetch_url(page_link)
+        article_content = extract(raw_article, output_format="xml")
+        
+        article = Article(page_title, page_description, page_timestamp, page_link, article_content)
+        print(article)
+
+        # From here push each individual piece to the weviate databse
+        break
+
+
+
+if __name__ == "__main__":
+    main()
