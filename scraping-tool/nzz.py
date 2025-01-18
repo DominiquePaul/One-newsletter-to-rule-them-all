f0a444c0b6f4e2bea4219e1aae84fe75e2bd9331
marcandrewessner <94740952+marcandrewessner@users.noreply.github.com>
2025-01-18 16:02:01 +0100

    nzz scraper

---
+++ scraping-tool/nzz.py
@@ -0,0 +1,25 @@
+# This file is used to scrape the NZZ articles
+from trafilatura import fetch_url, extract
+from feedparser import parse as parse_feed
+
+
+def main():
+
+    nzz_feed_link = "https://www.nzz.ch/technologie.rss"
+    nzz_feed = parse_feed(nzz_feed_link)
+    
+    for article in nzz_feed.entries:
+        page_title = article.title
+        page_description = article.description
+        page_link = article.link
+        # Stop at only one article
+        raw_article = fetch_url(page_link)
+        article = extract(raw_article, output_format="xml")
+        print(article)
+        break
+    
+
+
+
+if __name__ == "__main__":
+    main()
